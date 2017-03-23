c640e47b0f1b357ffedbfef39ded61c81c457862
Mahmudul Faisal Al Ameen <mahmudulfaisal@gmail.com>
2017-03-24 00:42:12 +0800

    3

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.static(__dirname + '/public'));
 //  app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+  response.render('index');
 });
 
 app.listen(app.get('port'), function() {
