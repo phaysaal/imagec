bc0ee6e0a1756c374692d7087ab5af1c3f1ea024
Mahmudul Faisal Al Ameen <mahmudulfaisal@gmail.com>
2017-03-28 01:08:34 +0800

    20

--- index.js
+++ index.js
@@ -1,6 +1,47 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
+//***************
+
+var gres = null;
+var gdata = null;
+
+var WebSocketClient = require('websocket').client;
+var client = new WebSocketClient();
+
+client.on('connectFailed', function(error) {
+    console.log('Connect Error: ' + error.toString());
+});
+
+client.on('connect', function(connection) {
+    connection.on('error', function(error) {
+        console.log("Connection Error: " + error.toString());
+    });
+    connection.on('message', function(message) {
+        if (gres != null) {
+            if (message.type === 'utf8') {
+                gres.send(message.utf8Data);
+                gdata = null;
+            } 
+        }    
+        connection.close();
+    });
+    
+    function sendData() {
+        if (connection.connected) {
+            var str = JSON.stringify({link: gdata.link, agent: gdata.agent});
+            connection.sendUTF(str);
+            //setTimeout(sendNumber, 6000);        
+        }
+    }
+    if (gdata != null && gres != null) {
+        sendData();
+    }
+});
+
+//***************
+app.use(bodyParser.json());
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
@@ -18,8 +59,9 @@ app.get('/ws', function(request, response) {
 } );
 
 app.post('/ws', function(request, response) {
-    var str = JSON.stringify({status: 409, imagelink: "", title: "", description: "K! It's cool!!"});
-    response.send(str);
+    gres = response;
+    gdata = request.body;
+    client.connect('ws://182.55.206.183:8010/', 'image-extraction');
 } )
 
 app.listen(app.get('port'), function() {
